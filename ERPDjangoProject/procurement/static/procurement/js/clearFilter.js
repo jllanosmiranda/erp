900f2984991553740ad3d4365444379958b5a101
Juan Llanos <llanosmirandajuan@gmail.com>
2025-07-11 04:23:47 -0500

    improve supplier product templates and forms with new product handling features

--- ERPDjangoProject/procurement/static/procurement/js/clearFilter.js
+++ ERPDjangoProject/procurement/static/procurement/js/clearFilter.js
@@ -9,11 +9,28 @@ function toggleEdit() {
 
     const div_view = document.getElementById('products-view');
     div_view.style.display = "none";
+
+    const div_add = document.getElementById('products-add');
+    div_add.style.display = "none";
 }
 
 function toggleView() {
     const div_view = document.getElementById('products-view');
     div_view.style.display = "block";
+
+    const div_edit = document.getElementById('products-edit');
+    div_edit.style.display = "none";
+
+    const div_add = document.getElementById('products-add');
+    div_add.style.display = "none";
+}
+
+function toggleAdd() {
+    const div_add = document.getElementById('products-add');
+    div_add.style.display = "block";
+
+    const div_view = document.getElementById('products-view');
+    div_view.style.display = "none";
 
     const div_edit = document.getElementById('products-edit');
     div_edit.style.display = "none";
@@ -30,5 +47,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const button_view = document.getElementById('button-view');
     button_view.addEventListener('click', () => {
         toggleView();
+    });
+
+    const button_add = document.getElementById('button-add');
+    button_add.addEventListener('click', () => {
+        toggleAdd();
     });
 });
