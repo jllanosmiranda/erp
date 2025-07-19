fdf87d535ae4be9a13d693271e960a14bcd91ce6
Juan Llanos <llanosmirandajuan@gmail.com>
2025-07-19 18:19:13 -0500

    put supplier details views and supplier details edit form in the same page

--- ERPDjangoProject/procurement/views/suppliers.py
+++ ERPDjangoProject/procurement/views/suppliers.py
@@ -9,6 +9,8 @@ from ..filters import SupplierProductFilter
 from ..forms import SupplierForm, SupplierContactSet, SupplierBankSet, ProductSupplierFormSet
 from ..models import Supplier, SupplierProduct
 
+log = logging.getLogger(__name__)
+
 
 def suppliers(request):
     if request.method == 'POST':
@@ -27,6 +29,7 @@ def suppliers(request):
 
 def view_supplier_details(request, supplier_id):
     print("request get",request.GET, flush=True)
+    log.info(f"supplier id {supplier_id}")
     supplier = Supplier.objects.get(id=supplier_id)
     products = supplier.products.all()
     products_filter = SupplierProductFilter(request.GET, queryset=products)
@@ -40,28 +43,46 @@ def view_supplier_details(request, supplier_id):
     form_id = None
     if request.method == 'POST':
         form_id = request.POST.get('form_id')
+        supplier_form = SupplierForm(request.POST, instance=supplier)
         print("request post",request.POST, flush=True)
-        product_form_set = ProductSupplierFormSet(request.POST,instance=supplier)
-        if product_form_set.is_valid():
-            logging.info("valid form set")
-            product_form_set.save()
-            url = reverse('supplier_details', kwargs={'supplier_id': supplier_id})
-            params = {'form_id': form_id}
-            params.update(filter_data)
-            url = f"{url}?{urlencode(params)}"
+        if form_id == "supplier-basic-information":
+            if supplier_form.is_valid():
+                supplier_form.save()
+                url = reverse('supplier_details', kwargs={'supplier_id': supplier_id})
+                params = {'form_id': form_id}
+                params.update(filter_data)
+                url = f"{url}?{urlencode(params)}"
+                return redirect(url)
+            else:
+                log.info("invalid supplier")
+                log.info(supplier_form.errors)
 
-            return redirect(url)
-        else:
-            logging.info("invalid form set")
-            logging.info(product_form_set.errors)
-            logging.info(product_form_set.non_form_errors())
+        product_form_set = ProductSupplierFormSet(request.POST,instance=supplier)
+        if form_id == "supplier_products":
+            if product_form_set.is_valid():
+                logging.info("valid form set")
+                product_form_set.save()
+                url = reverse('supplier_details', kwargs={'supplier_id': supplier_id})
+                params = {'form_id': form_id}
+                params.update(filter_data)
+                url = f"{url}?{urlencode(params)}"
+
+                return redirect(url)
+            else:
+                logging.info("invalid form set")
+                logging.info(product_form_set.errors)
+                logging.info(product_form_set.non_form_errors())
 
 
     else:
+        supplier_form = SupplierForm(instance=supplier)
         product_form_set = ProductSupplierFormSet(instance=supplier)
         form_id = request.GET.get('form_id')
 
 
+    log.info(f"form id {form_id}")
+    log.info(f"form supplier {supplier_form.errors}")
+    log.info(f"go to get path")
     paginator = Paginator(products_filter.qs, 10)
     page = request.GET.get('page')
     try:
@@ -85,6 +106,7 @@ def view_supplier_details(request, supplier_id):
                                               queryset=suppliers_objects)
     context = {
         "supplier": supplier,
+        "supplier_form": supplier_form,
         "filter": products_filter,
         "products": suppliers_objects,
         'products_forms': product_form_set,
