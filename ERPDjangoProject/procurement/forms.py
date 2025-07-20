09683b8339123a1d49ecfbb688dba02a647965eb
Juan Llanos <llanosmirandajuan@gmail.com>
2025-07-20 18:05:54 -0500

    testing field_order

--- ERPDjangoProject/procurement/forms.py
+++ ERPDjangoProject/procurement/forms.py
@@ -30,6 +30,7 @@ class SupplierForm(ModelForm):
     class Meta:
         model = Supplier
         fields = '__all__'
+        field_order = ['name', 'ruc', 'address', 'phone', 'email', 'website']
 
     def clean_website(self):
         website = self.cleaned_data.get("website")
