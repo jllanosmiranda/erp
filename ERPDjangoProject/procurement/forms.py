ebe5bb80c818dd89896f03f1621eb087a9dd61c4
Juan Llanos <llanosmirandajuan@gmail.com>
2025-07-21 19:32:18 -0500

    the order of the supplier form is define in the class SupplierForm instead of the template

--- ERPDjangoProject/procurement/forms.py
+++ ERPDjangoProject/procurement/forms.py
@@ -30,7 +30,10 @@ class SupplierForm(ModelForm):
     class Meta:
         model = Supplier
         fields = '__all__'
-        field_order = ['name', 'ruc', 'address', 'phone', 'email', 'website']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.order_fields(['name', 'ruc', 'address', 'phone', 'email', 'website'])
 
     def clean_website(self):
         website = self.cleaned_data.get("website")
