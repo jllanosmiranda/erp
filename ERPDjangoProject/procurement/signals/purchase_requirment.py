601134c61b14297b28c80727b722ccc678d9c822
Juan Llanos <llanosmirandajuan@gmail.com>
2025-08-20 23:14:25 -0500

    refactor signals: extract supplier product update logic into reusable function
    - Move supplier product update logic to `update_supplier_base_on_purchase_requirement_item` for improved reusability.
    - Update `post_save` and `pre_save` signals to use the new function, reducing code duplication and enhancing maintainability.

--- ERPDjangoProject/procurement/signals/purchase_requirment.py
+++ ERPDjangoProject/procurement/signals/purchase_requirment.py
@@ -18,33 +18,28 @@ def create_purchase_requirement_event(sender, instance, created, **kwargs):
 def update_purchase_requirement_event(sender, instance, **kwargs):
     update_event(sender, instance, PurchaseRequirementEvent)
 
+def update_supplier_base_on_purchase_requirement_item(purchase_requirement_item: PurchaseRequirementItems,):
+    supplier_product = SupplierProduct.objects.get(pk=purchase_requirement_item.supplier_product.pk)
+    supplier_product._changed_by = purchase_requirement_item._changed_by
+    if supplier_product.price != purchase_requirement_item.price:
+        supplier_product.price=purchase_requirement_item.price
+    if supplier_product.currency != purchase_requirement_item.currency:
+        supplier_product.currency=purchase_requirement_item.currency
+    if supplier_product.unit_of_measure != purchase_requirement_item.unit_of_measure:
+        supplier_product.unit_of_measure=purchase_requirement_item.unit_of_measure
+
+    supplier_product.save()
+
+
 @receiver(post_save, sender=PurchaseRequirementItems)
 def create_purchase_requirement_item_event(sender, instance, created, **kwargs):
     create_event(sender, instance, created, PurchaseRequirementItemEvent)
-    supplier_product = SupplierProduct.objects.get(pk=instance.supplier_product.pk)
-    supplier_product._changed_by = instance._changed_by
-    if supplier_product.price != instance.price:
-        supplier_product.price=instance.price
-    if supplier_product.currency != instance.currency:
-        supplier_product.currency=instance.currency
-    if supplier_product.unit_of_measure != instance.unit_of_measure:
-        supplier_product.unit_of_measure=instance.unit_of_measure
-
-    supplier_product.save()
+    update_supplier_base_on_purchase_requirement_item(instance)
 
 
 
 @receiver(pre_save, sender=PurchaseRequirementItems)
 def update_purchase_requirement_item_event(sender, instance, **kwargs):
     update_event(sender, instance, PurchaseRequirementItemEvent)
+    update_supplier_base_on_purchase_requirement_item(instance)
 
-    supplier_product = SupplierProduct.objects.get(pk=instance.supplier_product.pk)
-    supplier_product._changed_by = instance._changed_by
-    if supplier_product.price != instance.price:
-        supplier_product.price = instance.price
-    if supplier_product.currency != instance.currency:
-        supplier_product.currency = instance.currency
-    if supplier_product.unit_of_measure != instance.unit_of_measure:
-        supplier_product.unit_of_measure = instance.unit_of_measure
-
-    supplier_product.save()
